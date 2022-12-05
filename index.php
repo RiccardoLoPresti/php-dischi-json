a005ff2455583aad710174e22550f652fb49323e
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-12-05 14:22:12 +0100

    [add] layout and css

--- index.php
+++ index.php
@@ -8,18 +8,67 @@
     <link rel="stylesheet" href="./css/style.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Vue -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!-- axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- MainScript -->
     <script defer src="./js/main.js"></script>
-    
+
     <title>PHP Dischi JSON</title>
 </head>
 <body>
-    <div id="app">
-        
+    <div class="wrapper">
+        <div id="app">
+
+            <header>
+
+                <div class="container h-100">
+                    <div class="row h-100">
+                        <div class="col h-100">
+
+                            <div class="logo d-flex align-items-center h-100">
+                                <i class="fa-brands fa-spotify"></i>
+                                <h1 class="mx-3 text-uppercase fw-bold">Spotify Wrapped</h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </header>
+
+            <main>
+
+                <div class="container mt-4 h-100">
+                    <div class="row h-100">
+                        <div class="col h-100">
+
+                            <div class="card-wrapper d-flex flex-wrap justify-content-center">
+
+                                <div class="rl-card text-center">
+                                    <div class="box-img">
+                                        <img src="https://www.runningin.info/wp-content/uploads/2018/07/no-image.jpg" alt="ddd">
+                                    </div>
+                                    <div class="text">
+                                        <h3>Title</h3>
+                                        <p>artista</p>
+                                        <h5>anno</h5>
+                                    </div>
+                                </div>
+                                
+                                
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+            
+        </div>
     </div>
+    
+    
 </body>
 </html>
